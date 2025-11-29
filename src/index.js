442e4dc7040e270ff5a1643eb91dd43c7737cf5d
Tharunkumar K <tharunkumark42007@gmail.com>
2025-11-29 20:04:49 +0530

    DELETE

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-);
