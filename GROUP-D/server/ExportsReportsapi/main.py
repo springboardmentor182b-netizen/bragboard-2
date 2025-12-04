d520c7040598995e481743cdf0e3bfca1b2d25b9
taladameghana <taladameghana00@gmail.com>
2025-12-04 12:04:32 +0530

    Initialize FastAPI application for Reporting API

---
+++ GROUP-D/server/ExportsReportsapi/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from database import Base, engine
+from routes import report_routes
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Reporting Shoutouts API")
+
+app.include_router(report_routes.router)
