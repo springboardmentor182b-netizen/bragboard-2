5b9c55a8fb8a776f6222407a40ea3a8a4b2d9cc4
Geya123 <geyasree173@gmail.com>
2025-12-07 14:30:09 +0530

    Delete GROUP-D/server/reporting_shoutouts_api directory

--- GROUP-D/server/reporting_shoutouts_api/models.py
+++
@@ -1,9 +0,0 @@
-# Temporary in-memory database (list)
-shoutouts_db = []
-
-class ShoutoutModel:
-    def __init__(self, id, employee_id, message, status="Pending"):
-        self.id = id
-        self.employee_id = employee_id
-        self.message = message
-        self.status = status
