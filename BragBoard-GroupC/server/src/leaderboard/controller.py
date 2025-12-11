245c45f07980df7b4e8e5ff3d9fea99b31cc57a9
ArshitRawat <arshitrawat2704@gmail.com>
2025-12-11 11:35:14 +0530

    Update controller.py

--- BragBoard-GroupC/server/src/leaderboard/controller.py
+++ BragBoard-GroupC/server/src/leaderboard/controller.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter
-from .service import get_leaderboard
+from fastapi import APIRouter, Depends
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.leaderboard.models import get_db
+from src.leaderboard.models import Leaderboard
 
 router = APIRouter(
     prefix="/leaderboard",
@@ -7,5 +10,17 @@ router = APIRouter(
 )
 
 @router.get("/")
-def leaderboard():
-    return get_leaderboard()
+async def get_leaderboard(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Leaderboard).order_by(Leaderboard.points.desc()))
+    rows = result.scalars().all()
+
+    leaderboard = [
+        {
+            "rank": idx + 1,
+            "username": row.username,
+            "points": row.points
+        }
+        for idx, row in enumerate(rows)
+    ]
+
+    return leaderboard
