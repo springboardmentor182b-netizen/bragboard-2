df3fafe418a14aa9137e891c00b7c70a4fac6a7e
ArshitRawat <arshitrawat2704@gmail.com>
2025-12-11 11:32:08 +0530

    No need for users.controller.py
    as it is being handled by the auth db

--- BragBoard-GroupC/server/src/api.py
+++ BragBoard-GroupC/server/src/api.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from .leaderboard.controller import router as leaderboard_router
-from .users.controller import router as users_router
 
 api_router = APIRouter()
 
 api_router.include_router(leaderboard_router)
-api_router.include_router(users_router)
