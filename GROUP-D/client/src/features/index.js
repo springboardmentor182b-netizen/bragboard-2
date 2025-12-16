87336c57eb763a1b32f85833d529e38885bb7505
Geya123 <geyasree173@gmail.com>
2025-12-16 18:19:38 +0530

    Delete GROUP-D/client/src/features/index.js

--- GROUP-D/client/src/features/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import ReportingShoutouts from "./ReportingShoutouts";
-import "./reporting.css";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<ReportingShoutouts />);
-
