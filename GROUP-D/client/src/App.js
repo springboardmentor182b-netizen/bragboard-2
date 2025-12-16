d17faeb0a6579fc88f7db0d478a1d0cb0d650443
Geya123 <geyasree173@gmail.com>
2025-12-16 18:13:06 +0530

    Update App.js

--- GROUP-D/client/src/App.js
+++ GROUP-D/client/src/App.js
@@ -1,1 +1,20 @@
+import React from "react";
+import Shoutouts from "./pages/Shoutouts";
+import MyReportedShoutouts from "./pages/MyReportedShoutouts";
+
+function App() {
+  return (
+    <div>
+      <h1>Employee Shoutouts</h1>
+
+      <Shoutouts />
+
+      <hr />
+
+      <MyReportedShoutouts />
+    </div>
+  );
+}
+
+export default App;
 
