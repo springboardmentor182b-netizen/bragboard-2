de60a3e8aa6a07ac3a44ca433015ba7ea718de86
Yashaswini Mannava <mannavayashaswini@gmail.com>
2025-12-17 12:54:43 +0530

    Delete GROUP-D/server/admin_routes.py

--- GROUP-D/server/admin_routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from controllers.admin_analytics import get_admin_analytics
-
-router = APIRouter()
-
-@router.get("/analytics")
-def fetch_analytics():
-    return get_admin_analytics()
