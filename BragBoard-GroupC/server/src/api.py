af33d7ea3330f3c4ba79e0dac3c5d0c5af42f095
Dhun25 <dhun2555@email.com>
2025-12-19 15:32:01 +0530

    Added Admin Dashboard Analytics API and UI integration

--- BragBoard-GroupC/server/src/api.py
+++ BragBoard-GroupC/server/src/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .leaderboard.controller import router as leaderboard_router
+from src.analytics.analytics_controller import router as analytics_router
 
 api_router = APIRouter()
 
 api_router.include_router(leaderboard_router)
+api_router.include_router(analytics_router)
