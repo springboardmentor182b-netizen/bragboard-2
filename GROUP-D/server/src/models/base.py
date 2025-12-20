aa0b8a3a19a8766c761d8b11ebbcc8ae7485a3d8
Tharunkumar K <tharunkumark42007@gmail.com>
2025-12-20 21:51:51 +0530

    Delete GROUP-D/server/src/models directory

--- GROUP-D/server/src/models/base.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base(DeclarativeBase):
-    """Base class for all ORM models."""
-    pass
--- GROUP-D/server/src/models/shoutout_model.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class Shoutout(BaseModel):
-    userName: str
-    message: str
