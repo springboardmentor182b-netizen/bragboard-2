e2a6f3bf529f62a21f234203cb4ee8eee171f7e7
Tharunkumar K <tharunkumark42007@gmail.com>
2026-01-02 14:39:26 +0530

    Delete GROUP-D directory

--- GROUP-D/server/src/database/mongodb.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-MONGO_URL = "mongodb://localhost:27017"
-DB_NAME = "bragboard"
-
-client = AsyncIOMotorClient(MONGO_URL)
-db = client[DB_NAME]
--- GROUP-D/server/src/middleware/auth.py
+++
@@ -1,6 +0,0 @@
-from fastapi import Header, HTTPException
-
-async def verify_token(Authorization: str = Header(None)):
-    if Authorization != "Bearer valid-token":
-        raise HTTPException(401, "Invalid token")
-    return {"id": "123"}
